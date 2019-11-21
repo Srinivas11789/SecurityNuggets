65c67c9e061ac595891c9dd835d98515aba4f0d6
srinivas11789 <spg349@nyu.edu>
2019-11-20 21:02:20 -0800

    ritsec19 - web update

---
+++ captureTheFlag/Web/ritSec2019/potato/photos.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;margin:0px auto;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}
+.tg .tg-0lax{text-align:left;vertical-align:top}
+@media screen and (max-width: 767px) {.tg {width: auto !important;}.tg col {width: auto !important;}.tg-wrap {overflow-x: auto;-webkit-overflow-scrolling: touch;margin: auto 0px;}}</style>
+</head>
+<body>
+Welcome to our awesome gallery!</br>
+See recent uploaded pictures from our community, and feel free to rate or comment</br>
+<div class="tg-wrap"><table class="tg">
+<tr>
+<td class="tg-0lax">uploaded by 10_0_0_37.php.jpg<br><img src='uploads/10_0_0_37.php.jpg' width=100px></td>
+</tr>
+</table></div>
+</body>
+</html>
